4ebdfac8968733e957edabade405551ed6c231c0
karthik2804 <karthik.ganeshram@fermyon.com>
2023-09-11 10:49:13 -0700

    package as plugin for local tests
    Signed-off-by: karthik2804 <karthik.ganeshram@fermyon.com>

---
+++ create_plugin.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cargo build --release
+cp target/release/spin-cloud-gpu cloud-gpu 
+tar -czvf cloud-gpu.tar.gz cloud-gpu cloud-gpu-app/spin.toml cloud-gpu-app/target/spin-http-js.wasm
+
+cat <<EOT > cloud-gpu.json
+{
+    "name": "cloud-gpu",
+    "description": "A plugin to enable local AI development using cloud gpus.",
+    "homepage": "www.example.com",
+    "version": "0.1.0",
+    "spinCompatibility": ">=1.4",
+    "license": "Apache-2.0",
+    "packages": [
+        {
+            "os": "macos",
+            "arch": "aarch64",
+            "url": "file:$(pwd)/cloud-gpu.tar.gz",
+            "sha256": "$(sha256sum cloud-gpu.tar.gz | awk '{print $1;}')"
+        }
+    ]
+}
+EOT
